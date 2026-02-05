a85f5ee9f644dd61745ba4c82bce0bd6073a6e39
Hepytech Dev <hepytech@hallomepy.com>
2026-02-05 15:49:12 +0700

    fix: fix UI/UX

--- src/components/Common/Modal/Modal.tsx
+++ src/components/Common/Modal/Modal.tsx
@@ -19,14 +19,8 @@ interface ModalProps {
 }
 
 export default function ConfirmationModal({ isOpen, onClose, variant = "warning", title, description, cancelText = "Cancel", confirmText = "Ya",onConfirm, children }: ModalProps) {
-  const [confirmationInput, setConfirmationInput] = useState<string>("");
   const modalRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (!isOpen) {
-      setConfirmationInput("");
-    }
-  }, [isOpen]);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -78,12 +72,11 @@ export default function ConfirmationModal({ isOpen, onClose, variant = "warning"
 
   const handleConfirm = () => {
     onConfirm();
-    setConfirmationInput("");
   };
 
   return (
     <div className="fixed inset-0 bg-neutral-black/30 flex items-center justify-center z-30 p-4">
-      <div ref={modalRef} className="relative bg-neutral-white rounded-lg w-3/4 mx-auto flex flex-col justify-center items-center text-center px-6 py-8">
+      <div ref={modalRef} className="relative bg-neutral-white rounded-lg w-1/4 mx-auto flex flex-col justify-center items-center text-center px-6 py-8">
         <button type="button" onClick={onClose} className="absolute right-3 top-3 text-neutral-black cursor-pointer">
           <X width={24} height={24} />
         </button>
